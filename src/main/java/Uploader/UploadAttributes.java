fa404a27a0caf95d0795fb5b75051256da2c5161
ramanujsingh <ramanuj1st@gmail.com>
2024-09-17 00:41:34 +0530

    upload Design

--- src/main/java/Uploader/UploadAttributes.java
+++ src/main/java/Uploader/UploadAttributes.java
@@ -3,19 +3,21 @@ package Uploader;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Getter
 @Setter
 public class UploadAttributes {
 
+    @NotNull
     private String filePath;
     private String fileType;
     private Long timeWhenToUpload;
     private String caption;
     private List<String> hashTags;
     private String location;
-
-
+    @NotNull
+    private Credentials credentials;
 
 }
